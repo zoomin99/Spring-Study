c65a1c4b98c0614ff34e4d0e9d40bde89e3b6fec
zoomin99 <zoomin99@gmail.com>
2022-11-15 21:29:03 +0900

    김영한 스프링 스터디 2022/11/15
    의존관계 자동 주입 챕터 완료

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class MemberServiceImpl implements MemberService {
+    private final MemberRepository memberRepository;
+
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
