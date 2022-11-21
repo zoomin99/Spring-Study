b2737c4119f71781daad71c02457c3a756d52c67
zoomin99 <zoomin0128@gmail.com>
2022-11-21 21:03:33 +0900

    김영한 스프링 스터디 2022/11/21
    서블릿으로 웹 어플리케이션 만들기까지 완료

--- Spring MVC 1편/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ Spring MVC 1편/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 public class ServletApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServletApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServletApplication.class, args);
+    }
 
 }
--- Spring MVC 1편/servlet/src/main/java/hello/servlet/basic/HelloData.java
+++ Spring MVC 1편/servlet/src/main/java/hello/servlet/basic/HelloData.java
@@ -3,7 +3,8 @@ package hello.servlet.basic;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
 public class HelloData {
     private String username;
     private int age;
