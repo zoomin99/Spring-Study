e3c1c0dc5bc70f94a552b13525fdd55c6f40bba8
zoomin99 <zoomin0128@gmail.com>
2022-12-05 20:42:47 +0900

    김영한 스프링 스터디 2022/12/05
    스프링 MVC 시작하기까지 완료

--- Spring MVC 1편/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
+++ Spring MVC 1편/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
@@ -1,7 +1,9 @@
 package hello.servlet.web.frontcontroller.v4.controller;
+
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import hello.servlet.web.frontcontroller.v4.ControllerV4;
+
 import java.util.Map;
 public class MemberSaveControllerV4 implements ControllerV4 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
--- Spring MVC 1편/servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ Spring MVC 1편/servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MemberRepositoryTest {
     MemberRepository memberRepository = MemberRepository.getInstance();
 
