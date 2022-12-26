e2d3740dcc519880b1905b3e1e585f75fe4732b1
zoomin99 <zoomin0128@gmail.com>
2022-12-26 18:24:20 +0900

    김영한 스프링 스터디 2022/12/26
    Bean Validation - groups까지 완료

--- Spring MVC 2편/validation/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ Spring MVC 2편/validation/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//global validate설정을 위해선 implements WebMvcConfigurer
 public class ItemServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ItemServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceApplication.class, args);
+    }
 
+/*	@Override
+	public Validator getValidator() {
+		return new ItemValidator();
+	}*/
 }
